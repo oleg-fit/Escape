84610fed44fed163a966255cf43408bdcf6cc4fb
oleg-fit <oleg26-spb@rambler.ru>
2020-01-04 17:27:05 +0300

    Добавляет правки по замечаниям

--- build/js/script.js
+++ build/js/script.js
@@ -10,19 +10,12 @@ var linkBurgerMenu = document.querySelector('.header__burger');
 var bodyPage = document.querySelector('.body-js');
 
 var form = document.querySelector('.question-popap__form');
-var nameInput = form.querySelector('#name');
-var nameErrorBlock = form.querySelector('.error-name');
-var emailInput = form.querySelector('#mail');
-var emailErrorBlock = form.querySelector('.error-email');
-var textInput = form.querySelector('#question');
-var textErrorBlock = form.querySelector('.error-text');
-var agreeInput = form.querySelector('#agree');
-var textErrorAgree = form.querySelector('.error-agree');
 
 if (linkYourCity) {
 
   linkYourCity.addEventListener('click', function (evt) {
     evt.preventDefault();
+    bodyPage.classList.add('body-js--overlay');
     popapYourCity.classList.add('city-popap--show');
     overlayPopap.classList.add('overlay-popap--show');
   });
@@ -32,6 +25,7 @@ if (linkClosePopapYourCity) {
 
   linkClosePopapYourCity.addEventListener('click', function (evt) {
     evt.preventDefault();
+    bodyPage.classList.remove('body-js--overlay');
     popapYourCity.classList.remove('city-popap--show');
     overlayPopap.classList.remove('overlay-popap--show');
   });
@@ -40,6 +34,7 @@ if (linkClosePopapYourCity) {
     if (evt.keyCode === 27) {
       if (popapYourCity.classList.contains('city-popap--show')) {
         evt.preventDefault();
+        bodyPage.classList.remove('body-js--overlay');
         popapYourCity.classList.remove('city-popap--show');
         overlayPopap.classList.remove('overlay-popap--show');
       }
@@ -49,6 +44,7 @@ if (linkClosePopapYourCity) {
   overlayPopap.addEventListener('click', function (evt) {
     if (popapYourCity.classList.contains('city-popap--show')) {
       evt.preventDefault();
+      bodyPage.classList.remove('body-js--overlay');
       popapYourCity.classList.remove('city-popap--show');
       overlayPopap.classList.remove('overlay-popap--show');
     }
@@ -110,49 +106,59 @@ if (linkBurgerMenu) {
 
 
 // Валидация формы
+if (form) {
+  var nameInput = form.querySelector('#name');
+  var nameErrorBlock = form.querySelector('.error-name');
+  var emailInput = form.querySelector('#mail');
+  var emailErrorBlock = form.querySelector('.error-email');
+  var textInput = form.querySelector('#question');
+  var textErrorBlock = form.querySelector('.error-text');
+  var agreeInput = form.querySelector('#agree');
+  var textErrorAgree = form.querySelector('.error-agree');
 
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  function validation(e) {
+    if (nameInput.value === '') {
+      e.preventDefault();
+      nameErrorBlock.classList.add('error-name--show');
+      nameInput.classList.add('input--invalid');
+    } else {
+      nameErrorBlock.classList.remove('error-name--show');
+      nameInput.classList.remove('input--invalid');
+      nameInput.classList.add('input--valid');
+    }
+
+    if (validateEmail(emailInput.value) === false) {
+      e.preventDefault();
+      emailErrorBlock.classList.add('error-email--show');
+      emailInput.classList.add('input--invalid');
+    } else {
+      emailErrorBlock.classList.remove('error-email--show');
+      emailInput.classList.remove('input--invalid');
+      emailInput.classList.add('input--valid');
+    }
+
+    if (textInput.value === '') {
+      e.preventDefault();
+      textErrorBlock.classList.add('error-text--show');
+      textInput.classList.add('input--invalid');
+    } else {
+      textErrorBlock.classList.remove('error-text--show');
+      textInput.classList.remove('input--invalid');
+      textInput.classList.add('input--valid');
+    }
+
+    if (!agreeInput.checked) {
+      e.preventDefault();
+      textErrorAgree.classList.add('error-agree--show');
+    } else {
+      textErrorAgree.classList.remove('error-agree--show');
+    }
+  }
+
+  form.addEventListener('submit', validation);
 }
-
-function validation(e) {
-  if (nameInput.value === '') {
-    e.preventDefault();
-    nameErrorBlock.classList.add('error-name--show');
-    nameInput.classList.add('input--invalid');
-  } else {
-    nameErrorBlock.classList.remove('error-name--show');
-    nameInput.classList.remove('input--invalid');
-    nameInput.classList.add('input--valid');
-  }
-
-  if (validateEmail(emailInput.value) === false) {
-    e.preventDefault();
-    emailErrorBlock.classList.add('error-email--show');
-    emailInput.classList.add('input--invalid');
-  } else {
-    emailErrorBlock.classList.remove('error-email--show');
-    emailInput.classList.remove('input--invalid');
-    emailInput.classList.add('input--valid');
-  }
-
-  if (textInput.value === '') {
-    e.preventDefault();
-    textErrorBlock.classList.add('error-text--show');
-    textInput.classList.add('input--invalid');
-  } else {
-    textErrorBlock.classList.remove('error-text--show');
-    textInput.classList.remove('input--invalid');
-    textInput.classList.add('input--valid');
-  }
-
-  if (!agreeInput.checked) {
-    e.preventDefault();
-    textErrorAgree.classList.add('error-agree--show');
-  } else {
-    textErrorAgree.classList.remove('error-agree--show');
-  }
-}
-
-form.addEventListener('submit', validation);
